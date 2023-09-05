54f70db787570117f5fa3e9e4052b64b093d989b
unknown <asmamughal097@gmail.com>
2023-09-05 12:21:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useEffect } from 'react';
+import Aos from 'aos';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Portfolio from './components/Portfolio';
+import Testimonial from './components/Testimonial';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+<Navbar />
+<Home />
+<Skills />
+<Portfolio />
+<Testimonial />
+<Contact />
+<Footer />
     </div>
   );
 }
